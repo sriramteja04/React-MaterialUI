d4b4ba1f1f992949c549d378a8a00dd13ed58e29
Sri Ram Komerisetti <s531524@nwmissouri.edu>
2019-10-24 17:13:25 -0500

    succesfully navigates to new password page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import Login from './components/Auth/login/Login';
-import Home from './components/Home/Home';
 import PrivateRouter from './components/Routers/PrivateRouter';
 import NotFound from './components/Layouts/NotFound';
 import { getCurrentUser } from './modules/Auth/Login/actions';
 import './scss/style.scss';
 import Layout from './components/Layouts/index';
+
+import Login from './components/Auth/login/Login';
+import Home from './components/Home/Home';
+import NewPassword from './components/Auth/New Password/NewPassword';
 
 class App extends React.Component {
   async componentDidMount() {
@@ -22,6 +24,7 @@ class App extends React.Component {
         <CssBaseline />
         <Switch>
           <Route exact path="/" component={Login} />
+          <Route exact path={'/newPassword'} component={NewPassword} />
           <Layout>
             <PrivateRouter exact path="/home" component={Home} />
           </Layout>
